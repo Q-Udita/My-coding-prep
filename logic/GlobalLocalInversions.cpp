50c93fd21b69f5550e5318278a332ed8877b40b3
Udita Biswas <ishabiswas@gmail.com>
2018-01-28 11:03:55 +0530

    Update GlobalLocalInversions.cpp

--- logic/GlobalLocalInversions.cpp
+++ logic/GlobalLocalInversions.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int numJewelsInStones(string J, string S) {
+    bool isIdealPermutation(vector<int>& A) {
         
-        unordered_map<char,int> l;
-        int i,c=0;
-        int s1=J.size();
-        int s2=S.size();
-        for(i=0;i<s1;i++) {
-            l[J[i]]++;
+       for (int i = 0; i < A.size(); ++i) {
+            if (abs(A[i] - i) > 1) return false;
         }
-        for(i=0;i<s2;i++) {
-            if(l[S[i]]) 
-                c++;
-        }
-        return c;
+	return true;
     }
 };
