28706f9c3029f6f862964146f7207fadc7748f4b
Udita Biswas <ishabiswas@gmail.com>
2018-05-17 16:54:06 +0530

    Update closestsumto0.cpp
    https://practice.geeksforgeeks.org/problems/two-numbers-with-sum-closest-to-zero/0

--- arrays/closestsumto0.cpp
+++ arrays/closestsumto0.cpp
@@ -1,53 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <algorithm>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	int t,n,i,num1,num2,num3,num4,l=0;;
+int main()
+ {
+	int t;
 	cin>>t;
-	while(t--) {
-	    l=0;
+	while(t--)
+	{
+	    int n,i,j,k,l;
 	    cin>>n;
 	    vector<int> v(n);
-	    for(i=0;i<n;i++) {
+	    for(i=0;i<n;i++)
 	        cin>>v[i];
-	    }
 	    sort(v.begin(),v.end());
-	    int j=0,k=n-1;
-	    int sum,possum=LONG_MAX,negsum=LONG_MIN;
-	    while(j<k) {
-	       sum=v[j]+v[k];
-	       if(sum>0) {
-	           if(sum<possum) {
-	               possum=sum;
-	               num1=j;
-	               num2=k;
-	               k--;
-	           }
-	       }
-	       else if(sum<0){
-	           if(sum>negsum) {
-	               negsum=sum;
-	               num3=j;
-	               num4=k;
-	               j++;
-	           }
-	       }
-	       else {
-	           cout<<v[j]<<" "<<v[k]<<endl;
-	           l=1; break;
-	       }
+	    int m=INT_MAX;
+	    i=0;j=n-1;
+	    while(i<j)
+	    {
+	        int temp=v[i]+v[j];
+	        if(abs(temp) < abs(m))
+	        {
+	            m=temp;
+	            k=i;
+	            l=j;
+	        }
+	        if(temp<0)
+	        i++;
+	        else j--;
 	    }
-	    
-	    if(l==0) {
-	    if(possum<abs(negsum))
-	     cout<<v[num1]<<" "<<v[num2]<<endl;
-	    else cout<<v[num3]<<" "<<v[num4]<<endl; 
-	    }
+	    cout<<v[k]<<" "<<v[l]<<endl;
 	}
 	return 0;
 }
-
